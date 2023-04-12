cc9e346cbf762849356324445548d630af2ea5bd
rossamck <ross.mckeown1@gmail.com>
2023-04-12 18:11:55 +0100

    updated burger menu animation

---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,25 @@
+// BurgerMenu.jsx
+import React from "react";
+
+function BurgerMenu({ open }) {
+    return (
+        <div className="relative w-7 h-7">
+          <span
+            className={`absolute left-0 w-full h-0.5 bg-white rounded transition-all duration-300 ${
+              open ? "top-3 w-0 left-1/2" : "top-0"
+            }`}
+          ></span>
+          <span
+            className={`absolute left-0 w-full h-0.5 bg-white rounded transition-all duration-300 ${
+              open ? "top-3 transform rotate-45" : "top-2 "
+            }`}
+          ></span>
+          <span
+            className={`absolute left-0 w-full h-0.5 bg-white rounded transition-all duration-300 ${
+              open ? "top-3 transform -rotate-45" : "top-4"
+            }`}
+          ></span>
+        </div>
+      );
+    }
+export default BurgerMenu;

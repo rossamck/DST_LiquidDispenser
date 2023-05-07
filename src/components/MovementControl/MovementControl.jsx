a61aced9738930bdfc122d06229c0b16ca5bb40c
Ross McKeown <ross@Rosss-MacBook-Air.local>
2023-05-07 10:47:37 +0100

    added new counter

--- src/components/MovementControl/MovementControl.jsx
+++ src/components/MovementControl/MovementControl.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   FiArrowUp,
   FiArrowDown,
@@ -16,31 +16,89 @@ const MovementControl = ({
 }) => {
   const [activeButton, setActiveButton] = useState(null);
 
+  // Inside MovementControl component, add these new state variables
+  const [buttonPressCounts, setButtonPressCounts] = useState({
+    up: 0,
+    down: 0,
+    left: 0,
+    right: 0,
+    pageUp: 0,
+    pageDown: 0,
+  });
+
+  const incrementButtonPressCount = (buttonName) => {
+    setButtonPressCounts((prevCounts) => ({
+      ...prevCounts,
+      [buttonName]: prevCounts[buttonName] + 1,
+    }));
+  };
+
+  const resetButtonPressCounts = () => {
+    setButtonPressCounts({
+      up: 0,
+      down: 0,
+      left: 0,
+      right: 0,
+      pageUp: 0,
+      pageDown: 0,
+    });
+  };
+
+  const wrappedOnXUp = useCallback(() => {
+    onXUp();
+    incrementButtonPressCount("right");
+  }, [onXUp]);
+
+  const wrappedOnXDown = useCallback(() => {
+    onXDown();
+    incrementButtonPressCount("left");
+  }, [onXDown]);
+
+  const wrappedOnYUp = useCallback(() => {
+    onYUp();
+    incrementButtonPressCount("up");
+  }, [onYUp]);
+
+  const wrappedOnYDown = useCallback(() => {
+    onYDown();
+    incrementButtonPressCount("down");
+  }, [onYDown]);
+
+  const wrappedOnZUp = useCallback(() => {
+    onZUp();
+    incrementButtonPressCount("pageUp");
+  }, [onZUp]);
+
+  const wrappedOnZDown = useCallback(() => {
+    onZDown();
+    incrementButtonPressCount("pageDown");
+  }, [onZDown]);
+
   useEffect(() => {
     const handleKeyDown = (event) => {
       switch (event.key) {
         case "ArrowUp":
-          onYUp();
+          wrappedOnYUp();
           setActiveButton("up");
           break;
         case "ArrowDown":
-          onYDown();
+          wrappedOnYDown();
           setActiveButton("down");
           break;
         case "ArrowLeft":
-          onXDown();
+          wrappedOnXDown();
           setActiveButton("left");
           break;
         case "ArrowRight":
-          onXUp();
+          wrappedOnXUp();
           setActiveButton("right");
           break;
         case "PageUp":
-          onZUp();
+          wrappedOnZUp();
           setActiveButton("page-up");
           break;
         case "PageDown":
-          onZDown();
+          wrappedOnZDown();
           setActiveButton("page-down");
           break;
         default:
@@ -59,7 +117,7 @@ const MovementControl = ({
       document.removeEventListener("keydown", handleKeyDown);
       document.removeEventListener("keyup", handleKeyUp);
     };
-  }, [onXUp, onXDown, onYUp, onYDown, onZUp, onZDown]);
+  }, [wrappedOnXUp, wrappedOnXDown, wrappedOnYUp, wrappedOnYDown, wrappedOnZUp, wrappedOnZDown]);
 
   const getButtonClassName = (name) => {
     const baseClassName =
@@ -75,36 +133,54 @@ const MovementControl = ({
       <div className="grid grid-cols-3 grid-rows-2 gap-4 mb-4">
         <button
           className={getButtonClassName("up") + " col-start-2 row-start-1"}
-          onClick={onYUp}
+          onClick={wrappedOnYUp}
         >
           <FiArrowUp className="text-gray-800" size={24} />
         </button>
         <button
           className={getButtonClassName("left") + " col-start-1 row-start-2"}
-          onClick={onXDown}
+          onClick={wrappedOnXDown}
         >
           <FiArrowLeft className="text-gray-800" size={24} />
         </button>
         <button
           className={getButtonClassName("down") + " col-start-2 row-start-2"}
-          onClick={onYDown}
+          onClick={wrappedOnYDown}
         >
           <FiArrowDown className="text-gray-800" size={24} />
         </button>
         <button
           className={getButtonClassName("right") + " col-start-3 row-start-2"}
-          onClick={onXUp}
+          onClick={wrappedOnXUp}
         >
           <FiArrowRight className="text-gray-800" size={24} />
         </button>
       </div>
       <div className="flex justify-center items-center space-x-4 mb-4">
-        <button className={getButtonClassName("page-up")} onClick={onZUp}>
+        <button className={getButtonClassName("page-up")} onClick={wrappedOnZUp}>
           Up
         </button>
-        <button className={getButtonClassName("page-down")} onClick={onZDown}>
+        <button className={getButtonClassName("page-down")} onClick={wrappedOnZDown}>
           Down
         </button>
+      </div>
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold mb-2">Button Press Counts:</h2>
+        <ul>
+          {Object.entries(buttonPressCounts).map(([key, count]) => (
+            <li key={key} className="list-disc ml-6">
+              {key.charAt(0).toUpperCase() + key.slice(1)}: {count}
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-center">
+          <button
+            className="mt-4 p-2 w-24 rounded bg-gray-300 hover:bg-gray-400 active:bg-gray-500 text-gray-800 focus:outline-none reset-button"
+            onClick={resetButtonPressCounts}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/WellStatusTable/WellStatusTable.jsx
+++ src/components/WellStatusTable/WellStatusTable.jsx
@@ -1,76 +1,52 @@
-// WellStatusTable.jsx
-
-
 import React, { useRef, useEffect } from 'react';
 
-
-
 const WellStatusTable = ({ selectedWells, dispensingWell, completedWells }) => {
-  
-  const getStatus = (well) => {
-    if (completedWells.includes(well.wellId)) {
-      return (
-        <span className="inline-flex items-center">
-          <i className="fas fa-check-circle text-green-500 mr-1"></i> Complete
-        </span>
-      );
-    } else if (well.wellId === dispensingWell) {
-      return (
-        <span className="inline-flex items-center">
-          <i className="fas fa-circle text-yellow-500 mr-1"></i> Dispensing
-        </span>
-      );
-    } else {
-      return "-";
+  const isCompleted = (well) => completedWells.includes(well.wellId);
+  const isDispensing = (well) => well.wellId === dispensingWell;
+
+  const dispensingWellRef = useRef(null);
+
+  useEffect(() => {
+    if (dispensingWellRef.current && dispensingWell) {
+      const parentDiv = dispensingWellRef.current.parentNode.parentNode.parentNode;
+      const offsetTop = dispensingWellRef.current.offsetTop;
+      const parentDivHeight = parentDiv.offsetHeight;
+      const rowHeight = dispensingWellRef.current.offsetHeight;
+
+      parentDiv.scrollTop = offsetTop - parentDivHeight / 2 + rowHeight / 2;
     }
-  };
-  
-const dispensingWellRef = useRef(null);
-
-useEffect(() => {
-  if (dispensingWellRef.current && dispensingWell) {
-    const parentDiv = dispensingWellRef.current.parentNode.parentNode.parentNode;
-    const offsetTop = dispensingWellRef.current.offsetTop;
-    const parentDivHeight = parentDiv.offsetHeight;
-    const rowHeight = dispensingWellRef.current.offsetHeight;
-
-    parentDiv.scrollTop = offsetTop - parentDivHeight / 2 + rowHeight / 2;
-  }
-}, [dispensingWell, selectedWells, completedWells]);
-
-
+  }, [dispensingWell, selectedWells, completedWells]);
 
   return (
     <div className='w-full h-full overflow-x-hidden overflow-y-auto'>
-    <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
-    <thead>
-  <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
-    <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left">Well ID</th>
-    <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left">Volume</th>
-    <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left">Source</th>
-    <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left w-32">Status</th>
-  </tr>
-</thead>
-
-<tbody className="table-tbody-scroll text-gray-600 text-sm font-light ">
-  {selectedWells.map((well, index) => (
-    <tr
-      key={well.wellId}
-      ref={well.wellId === dispensingWell ? dispensingWellRef : null}
-      className={index % 2 === 0 ? 'bg-gray-50' : ''}
-    >
-      <td className="py-2 px-2 border-b border-gray-200">{well.wellId}</td>
-      <td className="py-2 px-2 border-b border-gray-200">{well.volume} μL</td>
-      <td className="py-2 px-2 border-b border-gray-200">{well.sourceIndex}</td>
-      <td className="py-2 px-2 border-b border-gray-200">{getStatus(well)}</td>
-    </tr>
-  ))}
-</tbody>
-
-    </table>
+      <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
+        <thead>
+          <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
+            <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left">Well ID</th>
+            <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left">Volume</th>
+            <th className="py-3 px-2 border-b border-gray-200 font-semibold text-left">Source</th>
+          </tr>
+        </thead>
+        <tbody className="table-tbody-scroll text-gray-600 text-sm font-light ">
+          {selectedWells.map((well, index) => (
+            <tr
+              key={well.wellId}
+              ref={isDispensing(well) ? dispensingWellRef : null}
+              className={`
+                ${index % 2 === 0 ? 'bg-gray-50' : ''}
+                ${isCompleted(well) ? 'bg-green-100' : ''}
+                ${isDispensing(well) && !isCompleted(well) ? 'border-red-500 border-2' : ''}
+              `}
+            >
+              <td className="py-2 px-2 border-b border-gray-200">{well.wellId}</td>
+              <td className="py-2 px-2 border-b border-gray-200">{well.volume} μL</td>
+              <td className="py-2 px-2 border-b border-gray-200">{well.sourceIndex}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default WellStatusTable;
-

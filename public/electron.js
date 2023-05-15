dab7a39b466de94e518bf89cd946181e4069cf5c
Ross McKeown <ross@Rosss-MacBook-Air.local>
2023-05-15 14:30:10 +0100

    attempt 2

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 const server = express();
 server.use(express.static(`${__dirname}/../build`));
 server.listen(3000);
 
+let win;
+
 function createWindow() {
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -17,7 +20,37 @@ function createWindow() {
 
   win.maximize();
   win.loadURL('http://localhost:3000');
-
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  autoUpdater.checkForUpdatesAndNotify();
+  createWindow();
+});
+
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update available',
+    message: 'A new version of the app is available. Do you want to update now?',
+    buttons: ['Update', 'Later']
+  }).then(result => {
+    let buttonIndex = result.response;
+    if (buttonIndex === 0) {
+      autoUpdater.downloadUpdate();
+    }
+  });
+});
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update ready',
+    message: 'Install and restart now?',
+    buttons: ['Yes', 'Later']
+  }).then(result => {
+    let buttonIndex = result.response;
+    if (buttonIndex === 0) {
+      autoUpdater.quitAndInstall(false, true);
+    }
+  });
+});
